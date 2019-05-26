4e4f8b65e188aaad00b8b1622326f4be025f42ae
Madhur Gupta <madhurnitc@gmail.com>
2019-05-26 12:49:05 -0400

    May26,2019

---
+++ src/org/learn/leetcode/easy/ValidPalindrome.java
@@ -0,0 +1,41 @@
+package org.learn.leetcode.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+
+
+        if(s == null || s.isEmpty()) {
+            return true;
+        }
+
+        int low = 0;
+        int high = s.length()-1;
+
+        while( low<=high) {
+            if(!Character.isLetterOrDigit(s.charAt(low))) {
+                low++;
+            }else if(!Character.isLetterOrDigit(s.charAt(high))) {
+                high--;
+            } else {
+
+                if(Character.toLowerCase(s.charAt(low)) != Character.toLowerCase(s.charAt(high))) {
+                    return false;
+                }
+
+                low++;
+                high--;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidPalindrome().isPalindrome("race a car"));
+    }
+}
---
+++ src/org/learn/org/learn/random/EnumExmaple.java
@@ -0,0 +1,27 @@
+package org.learn.org.learn.random;
+
+public class EnumExmaple {
+
+
+    public enum Cases {
+        Upper,
+        Lower,
+        Mixed
+    }
+
+    public void pirntCase(Cases cases) {
+
+        if(cases == Cases.Lower) {
+            System.out.println(cases.name());
+        }else if(cases == Cases.Upper) {
+            System.out.println(cases.name());
+        }else {
+            System.out.println(cases.name());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new EnumExmaple().pirntCase(Cases.Upper);
+    }
+}
